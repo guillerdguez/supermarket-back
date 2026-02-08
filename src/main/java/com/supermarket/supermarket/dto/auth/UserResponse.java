e6a18661dc3e4aee29ffceef06b296a0fe35db94
guillerdguez <guillermordguezrdguez@gmail.com>
2026-02-08 14:20:00 +0100

    feat: add authentication DTOs (LoginRequest, RegisterRequest, AuthResponse, UserResponse)

---
+++ src/main/java/com/supermarket/supermarket/dto/auth/UserResponse.java
@@ -0,0 +1,17 @@
+package com.supermarket.supermarket.dto.auth;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private Long id;
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String role;
+}
