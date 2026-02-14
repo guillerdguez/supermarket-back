3ab6de4bcd045c7becc199e8a62ce58f9550d4e3
guillerdguez <guillermordguezrdguez@gmail.com>
2026-02-14 17:30:00 +0100

    fix: correct method name in ProductRepository (findByStock)

--- src/main/java/com/supermarket/supermarket/repository/ProductRepository.java
+++ src/main/java/com/supermarket/supermarket/repository/ProductRepository.java
@@ -13,6 +13,6 @@ public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpec
 
     boolean existsByName(String name);
 
-    List<Product> findBystockLessThan(Integer stock);
+    List<Product> findByStockLessThan(Integer stock);
 
 }
