b82d2e86d58b974c9c391cc8f9afaa457e970015
guillerdguez <guillermordguezrdguez@gmail.com>
2026-02-16 21:41:57 +0100

    feat: implement InventoryService and refactor StockService to use branch logic

--- src/main/java/com/supermarket/supermarket/service/business/StockService.java
+++ src/main/java/com/supermarket/supermarket/service/business/StockService.java
@@ -3,11 +3,13 @@ package com.supermarket.supermarket.service.business;
 import java.util.List;
 
 import com.supermarket.supermarket.dto.saleDetail.SaleDetailRequest;
-import com.supermarket.supermarket.model.Product;
 import com.supermarket.supermarket.model.SaleDetail;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface StockService {
-    List<Product> validateAndReduceStockBatch(List<SaleDetailRequest> details);
+    @Transactional
+    void validateAndReduceStockBatch(List<SaleDetailRequest> details, Long branchId);
 
-    void restoreStockBatch(List<SaleDetail> existingDetails);
+    @Transactional
+    void restoreStockBatch(List<SaleDetail> details, Long branchId);
 }
