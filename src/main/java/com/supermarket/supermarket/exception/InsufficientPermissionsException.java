0ed83062ebe607bc9bee1dfc46401b6e2c6e001c
guillerdguez <guillermordguezrdguez@gmail.com>
2026-02-23 13:18:03 +0100

    refactor: remove @ResponseStatus from custom exceptions (now handled by GlobalExceptionHandler)

--- src/main/java/com/supermarket/supermarket/exception/InsufficientPermissionsException.java
+++ src/main/java/com/supermarket/supermarket/exception/InsufficientPermissionsException.java
@@ -1,9 +1,5 @@
 package com.supermarket.supermarket.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.FORBIDDEN)
 public class InsufficientPermissionsException extends RuntimeException {
     public InsufficientPermissionsException(String message) {
         super(message);
