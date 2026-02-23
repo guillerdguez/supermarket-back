19f26ddc78d513ebf61f9c5430ad2ab855452040
guillerdguez <guillermordguezrdguez@gmail.com>
2026-02-23 13:18:03 +0100

    refactor: remove @ResponseStatus from custom exceptions (now handled by GlobalExceptionHandler)

--- src/main/java/com/supermarket/supermarket/exception/RateLimitExceededException.java
+++ src/main/java/com/supermarket/supermarket/exception/RateLimitExceededException.java
@@ -1,9 +1,5 @@
 package com.supermarket.supermarket.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.TOO_MANY_REQUESTS)
 public class RateLimitExceededException extends RuntimeException {
     public RateLimitExceededException(String message) {
         super(message);
