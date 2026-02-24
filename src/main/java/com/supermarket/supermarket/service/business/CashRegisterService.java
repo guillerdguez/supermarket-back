e0226ee16cda6193ebf043037408665f4bd5db5f
guillerdguez <guillermordguezrdguez@gmail.com>
2026-02-24 15:20:32 +0100

    feat: add getRegisterEntityByBranch to CashRegisterService interface

--- src/main/java/com/supermarket/supermarket/service/business/CashRegisterService.java
+++ src/main/java/com/supermarket/supermarket/service/business/CashRegisterService.java
@@ -3,9 +3,15 @@ package com.supermarket.supermarket.service.business;
 import com.supermarket.supermarket.dto.cashregister.CashRegisterResponse;
 import com.supermarket.supermarket.dto.cashregister.CloseRegisterRequest;
 import com.supermarket.supermarket.dto.cashregister.OpenRegisterRequest;
+import com.supermarket.supermarket.model.CashRegister;
 
 public interface CashRegisterService {
     CashRegisterResponse openRegister(OpenRegisterRequest request);
+
     CashRegisterResponse closeRegister(Long registerId, CloseRegisterRequest request);
+
     CashRegisterResponse getCurrentRegisterByBranch(Long branchId);
+
+    CashRegister getRegisterEntityByBranch(Long branchId);
+
 }
